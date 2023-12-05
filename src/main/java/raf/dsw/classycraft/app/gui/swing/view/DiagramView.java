7b12f5eaa0c301ccfa0d8076b6f418a638b06b16
Filip Bajraktari <bajraktarifilip@gmail.com>
2023-12-05 10:25:27 +0100

    changed DiagramView

--- src/main/java/raf/dsw/classycraft/app/gui/swing/view/DiagramView.java
+++ src/main/java/raf/dsw/classycraft/app/gui/swing/view/DiagramView.java
@@ -1,46 +1,42 @@
 package raf.dsw.classycraft.app.gui.swing.view;
 
-import raf.dsw.classycraft.app.gui.swing.controller.debug.PaintTestAction;
-import raf.dsw.classycraft.app.gui.swing.view.painters.interclassPainters.InterclassPainter;
+import raf.dsw.classycraft.app.gui.swing.view.painters.ElementPainter;
 import raf.dsw.classycraft.app.model.ClassyRepository.Diagram;
-import raf.dsw.classycraft.app.model.elements.Interclass.ClassElement;
-import raf.dsw.classycraft.app.model.elements.Modifiers.AccessModifiers;
 import raf.dsw.classycraft.app.model.observerPattern.IListener;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DiagramView extends JPanel implements IListener {
 
     private Diagram diagram;
-    private boolean painterTestActionShouldBePerformed = false;
+    private List<ElementPainter> painters;
 
     public DiagramView(Diagram diagram){
         this.diagram = diagram;
+        painters = new ArrayList<>();
         diagram.addListener(this);
     }
 
     @Override
     public void update(Object notification) {
-        if (notification instanceof PaintTestAction) {
-            painterTestActionShouldBePerformed = true;
-        }
         repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D graphics2D = (Graphics2D) g;
+        for (ElementPainter painter: painters) {
+            painter.draw(graphics2D, 100, 100);
+        }
+        System.out.println("DiagramView paintComponent is being performed");
     }
 
     public Diagram getDiagram() {
         return diagram;
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        if (painterTestActionShouldBePerformed) {
-            Graphics2D graphics2D = (Graphics2D) g;
-            InterclassPainter interclassPainter = new InterclassPainter(new ClassElement("test class", diagram.getParent(), new Point(100, 100), AccessModifiers.PRIVATE, "test class name"));
-            interclassPainter.draw(graphics2D, 100, 100);
-            System.out.println("DiagramView paintComponent is being performed");
-            painterTestActionShouldBePerformed = false;
-        }
-    }
 }
