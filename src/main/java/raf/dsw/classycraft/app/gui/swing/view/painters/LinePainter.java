b62fb6b685ade36fa6f52768d815fdd5714276c6
Vladimir Ignjatijevic <vlada.ignjatije@gmail.com>
2023-12-09 14:18:30 +0100

    LinePainter is now again out of list of painters of DiagramView as LassoPainter is

--- src/main/java/raf/dsw/classycraft/app/gui/swing/view/painters/LinePainter.java
+++ src/main/java/raf/dsw/classycraft/app/gui/swing/view/painters/LinePainter.java
@@ -1,44 +1,36 @@
 package raf.dsw.classycraft.app.gui.swing.view.painters;
 
 import raf.dsw.classycraft.app.model.elements.Interclass.Interclass;
-import raf.dsw.classycraft.app.model.elements.LineElement;
 
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 
-public class LinePainter extends ElementPainter {
+public class LinePainter {
+    private Interclass interclass;
+    private Point currentPoint;
 
-    public LinePainter(LineElement diagramElement) {
-        super(diagramElement);
-    }
-
-    @Override
-    public void drawSelectionBox(Graphics2D graphics2D) {
+    public LinePainter(Interclass interclass) {
+        this.interclass = interclass;
+        this.currentPoint = interclass.getLocation();
 
     }
 
-    @Override
+
     public void draw(Graphics2D graphics2D) {
         Stroke previousStroke = graphics2D.getStroke();
         Color lastColor = graphics2D.getColor();
         graphics2D.setStroke(new BasicStroke(2));
         graphics2D.setColor(new Color(0, 0, 0));
         graphics2D.drawLine((int)getStartingPoint().getX(), (int)getStartingPoint().getY(), (int) getCurrentPoint().getX(), (int) getCurrentPoint().getY());
+        System.out.println("getStartingPoint() " + getStartingPoint());
+        System.out.println("getCurrentPoint() " + getCurrentPoint());
         graphics2D.setStroke(previousStroke);
         graphics2D.setColor(lastColor);
     }
 
-    @Override
-    public boolean elementAt(Point location) {
-        return false;
+    public void setCurrentPoint(Point currentPoint) {
+        this.currentPoint = currentPoint;
     }
-
-    private LineElement getLineElement() {return (LineElement) diagramElement;}
-
-    private Interclass getInterclass() {return getLineElement().getInterclassElement();}
-
-    private Point getCurrentPoint() {return  getLineElement().getCurrentPoint(); }
-
-    private Point getStartingPoint() {return getInterclass().getBestStartingPoint(getCurrentPoint()); }
+    private Point getCurrentPoint() {return  currentPoint; }
+    private Point getStartingPoint() {return interclass.getBestStartingPoint(currentPoint); }
 
 }
